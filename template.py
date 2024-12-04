289eed70923893648a0f59396568234b332330df
klakshay555 <badyanath223@gmail.com>
2024-12-04 17:53:07 +0530

    update

--- template.py
+++ template.py
@@ -18,6 +18,7 @@ list_of_files = [
     f"{project_name}/entity/config_entity.py",
     f"{project_name}/entity/artifact_entity.py",
     f"{project_name}/logger/__init__.py",
+    f"{project_name}/exception/__init__.py",
     f"{project_name}/pipeline/__init__.py",
     f"{project_name}/pipeline/training_pipeline.py",
     f"{project_name}/pipeline/prediction_pipeline.py",
